a473b7ab3302275e64be6495fed9cc2c60aac151
Ruben Vicario Gonzalez <ruben1586@gmail.com>
2015-03-23 11:38:53 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,122 @@
+var _ = require('lodash');
+
+
+var PENDING = 0;
+var FULFILLED = 1;
+var REJECTED = 2;
+
+/**
+ *
+ */
+var Promise = function(fn) {
+  var state = PENDING;
+  var value = null;
+  var handlers = [];
+
+  /**
+   * Called from resolve
+   */
+  var fulfill = function(result) {
+    state = FULFILLED;
+    value = result;
+    handlers.forEach(handle);
+    handlers = null;
+  };
+
+  /**
+   * Passed to doResolve. Call either from doResolve or resolve
+   */
+  var reject = function(error) {
+    state = REJECTED;
+    value = error;
+    handlers.forEach(handle);
+    handlers = null;
+  };
+
+  /**
+   * Passed to doResolve. Call from doResolve
+   */
+  var resolve = function(result) {
+    try {
+      var then = getThen(result);
+      if(then) {
+      	doResolve(then.bind(result), resolve, reject);
+        return;
+      }
+      fulfill(result);
+    } catch(err) {
+      reject(err);
+    }
+  };
+
+  /**
+   *
+   */
+  var handle = function(handler) {
+    if(state === PENDING) {
+      handlers.push(handler);
+    } else if(state === FULFILLED && typeof handler.onFulfilled === 'function') {
+      handler.onFulfilled(value);
+    } else if(state === REJECTED && typeof handler.onRejected === 'function') {
+      handler.onRejected(value);
+    }
+  };
+  
+  /**
+   *
+   */
+  this.done = function(onFulfilled, onRejected) {
+    //make it asynchronous, we want to run this after .done returns
+    setTimeout(function() {
+      handle({
+      	onFulfilled: onFulfilled,
+      	onRejected: onRejected
+      })
+    }, 0);
+  };
+
+  doResolve(fn, resolve, reject);
+};
+
+/**
+ * check if value passed is a promise or an actual value
+ */
+
+var getThen = function(value) {
+  var type = typeof value;
+  if(value && (type === 'object' || type === 'function')) {
+    var then = value.then;
+    if(typeof then === 'function') {
+      return then;
+    }
+  }
+  return null;
+};
+
+/**
+ *
+ */
+var doResolve = function(fn, onFulfilled, onRejected) {
+  var done = false;
+  //use try because fn is passed by user --> code not trusted, could break
+  try {
+  	/**
+     * Run function passed to API. Takes two functions: 
+     * One will take the result value that the user passes through API 
+     * The other will take the error that the user passes through API
+     */
+    fn(function(valuePassed) {
+      if(done) return;
+      done = true;
+      onFulfilled(valuePassed);
+    }, function(errorPassed) {
+      if(done) return;
+      done = true;
+      onRejected(errorPassed);
+    });
+  } catch(err) {
+    if(done) return;
+    done = true;
+    onRejected(err);
+  }
+};
